c4de747e3501c59c908b54d798db351d607e154b
Jim Bothén <jim.bothen@gmail.com>
2025-05-12 15:25:50 +0200

    Created mock playlist and successfully logged content in console

---
+++ script.js
@@ -0,0 +1,11 @@
+const playlists = [];
+
+const examplePlaylist = {
+  name: "Söndagsmys",
+  genre: "Pop",
+  artist: "Veronica Maggio",
+  songs: ["Jag kommer", "Snälla bli min"],
+};
+
+playlists.push(examplePlaylist);
+console.log(playlists);
